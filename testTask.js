4e9207f347b78c06bd77b443682ff56b783df2e2
VashAlex <47140236+VashAlex@users.noreply.github.com>
2019-02-12 10:15:01 +0200

    Add files via upload

---
+++ testTask.js
@@ -0,0 +1,55 @@
+var myObj;
+
+function Elem() {
+    var div = document.createElement('div');
+     div.className = 'wrap';
+    var img = document.createElement('img');
+    img.src = myObj[i].imageUrl;
+    var link = document.createElement('a');
+    link.className = 'link';
+     link.href = '';
+    link.appendChild(img);
+    div.appendChild(link);
+    demo.appendChild(div);
+    link.onclick = function (event) {
+        event.preventDefault();
+             
+     }
+    //  console.log(myObj[i]); 
+}
+ //    var backgr = document.createElement('div');
+        //      backgr.id = 'backgr';
+        //      div.appendChild(backgr);
+
+var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", 'test.json', true);
+    xhttp.send();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myObj = JSON.parse(this.responseText);
+       console.log(myObj);
+        var demo = document.getElementById("demo");
+       for(i=0; i<myObj.length; i+=1) {
+            var elem = new Elem(myObj[i]);
+       }
+      
+    }
+
+};
+
+
+
+var click = document.getElementById('btn');
+click.onclick = function(event) {
+    event.preventDefault();
+    console.log(myObj[1].id + ' ' + myObj[1].name) 
+}
+
+
+
+var bht = document.getElementsByTagName('img');
+console.log(bht.src)
+
+
